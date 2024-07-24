2323ce57647efb3699c245eaa04f0bb06c1aeb56
--set <elfoton@gmail.com>
2024-07-24 02:40:05 -0300

    feat: show tooltip con completed checkbox

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, Group, Paper, Title } from "@mantine/core";
+import { Checkbox, Group, Paper, Title, Tooltip } from "@mantine/core";
 import { TodoItem as TodoItemType } from "./types/TodoItem";
 import { ChangeEvent } from "react";
 
@@ -11,7 +11,20 @@ export function TodoItem({ todoItem, onChange }: TodoItemProps) {
   return (
     <Paper shadow="sm" p="xl" radius="lg">
       <Group wrap="nowrap">
-        <Checkbox checked={todoItem.completed} size="md" onChange={onChange} />
+        <Tooltip
+          label={
+            todoItem.completed
+              ? "Marcar como incompleta"
+              : "Marcar como completa"
+          }
+        >
+          <Checkbox
+            checked={todoItem.completed}
+            size="md"
+            onChange={onChange}
+          />
+        </Tooltip>
+
         <Title order={3} lineClamp={2}>
           {todoItem.title}
         </Title>
