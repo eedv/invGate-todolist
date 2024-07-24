508c13a8f86e5ab3f12ca54c4d2d231640c2451a
--set <elfoton@gmail.com>
2024-07-24 01:26:15 -0300

    feat: add TodoItem component and data type

---
+++ src/TodoItem.tsx
@@ -0,0 +1,21 @@
+import { Checkbox, Group, Paper, Title } from "@mantine/core";
+import { TodoItem as TodoItemType } from "./types/TodoItem";
+import { ChangeEvent } from "react";
+
+export interface TodoItemProps {
+  todoItem: TodoItemType;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export function TodoItem({ todoItem, onChange }: TodoItemProps) {
+  return (
+    <Paper shadow="sm" p="xl">
+      <Group wrap="nowrap">
+        <Checkbox checked={todoItem.completed} size="md" onChange={onChange} />
+        <Title order={3} lineClamp={2}>
+          {todoItem.title}
+        </Title>
+      </Group>
+    </Paper>
+  );
+}
