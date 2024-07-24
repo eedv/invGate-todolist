92e42dd38bede1ae2b96817a1f2dda48f3b0b021
--set <elfoton@gmail.com>
2024-07-24 11:10:03 -0300

    refactor: change layout to allow for lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import "@mantine/core/styles.css";
-
-import { Container, MantineProvider } from "@mantine/core";
+import { AppShell, Burger, Group, MantineProvider, Title } from "@mantine/core";
 import { TodoList } from "./TodoList";
+import { useDisclosure } from "@mantine/hooks";
 
 function App() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <MantineProvider>
-      <Container py="md">
-        <TodoList />
-      </Container>
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            <Title order={1}>Todo List</Title>
+          </Group>
+        </AppShell.Header>
+        <AppShell.Navbar>Navbar</AppShell.Navbar>
+        <AppShell.Main>
+          <TodoList />
+        </AppShell.Main>
+      </AppShell>
     </MantineProvider>
   );
 }
