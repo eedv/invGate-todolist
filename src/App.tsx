a7da8b19a815cafbea19784bcb7082377ba1e8e7
--set <elfoton@gmail.com>
2024-07-24 23:00:45 -0300

    feat: add router outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,35 @@
 import "@mantine/core/styles.css";
-import { AppShell, Burger, Group, MantineProvider, Title } from "@mantine/core";
-import { TodoList } from "./TodoList";
+import { AppShell, Burger, Group, NavLink, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { Outlet, NavLink as NavLinkRouter } from "react-router-dom";
 
-function App() {
+export function App() {
   const [opened, { toggle }] = useDisclosure();
 
   return (
-    <MantineProvider>
-      <AppShell
-        header={{ height: 60 }}
-        navbar={{
-          width: 300,
-          breakpoint: "sm",
-          collapsed: { mobile: !opened },
-        }}
-        padding="md"
-      >
-        <AppShell.Header>
-          <Group h="100%" px="md">
-            <Burger
-              opened={opened}
-              onClick={toggle}
-              hiddenFrom="sm"
-              size="sm"
-            />
-            <Title order={1}>Todo List</Title>
-          </Group>
-        </AppShell.Header>
-        <AppShell.Navbar>Navbar</AppShell.Navbar>
-        <AppShell.Main>
-          <TodoList />
-        </AppShell.Main>
-      </AppShell>
-    </MantineProvider>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Title order={1}>Todo List</Title>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar>
+        <NavLink component={NavLinkRouter} to="lists/1" label="lista 1" />
+        <NavLink component={NavLinkRouter} to="lists/2" label="lista 2" />
+        <NavLink component={NavLinkRouter} to="lists/3" label="lista 3" />
+      </AppShell.Navbar>
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
   );
 }
-
-export default App;
